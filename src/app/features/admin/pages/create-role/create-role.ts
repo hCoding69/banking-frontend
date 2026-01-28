f88383b0d670e29a175116bd8ab1c10534e7f0b7
hCoding69 <fatima.ezzahraa.qanoufi@wincocanada.com>
2026-01-28 15:24:34 +0100

    f

--- src/app/features/admin/pages/create-role/create-role.ts
+++ src/app/features/admin/pages/create-role/create-role.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Heading } from "../../../../shared/components/heading/heading";
+import { ActivatedRoute, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-create-role',
-  imports: [],
+  imports: [Heading],
   templateUrl: './create-role.html',
   styleUrl: './create-role.scss',
 })
 export class CreateRole {
+  title =  ''
+
+  constructor(route: ActivatedRoute) { 
+    this.title = route.snapshot.data['title'] 
+
+  }
 
 }
